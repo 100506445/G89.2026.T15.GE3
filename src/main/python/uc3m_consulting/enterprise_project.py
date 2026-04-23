1ea16e996b15135e55130a07fe10c88137ccba4b
Mohamed <100506445@alumnos.uc3m.es>
2026-04-23 10:37:04 +0200

    Refactor: fix pylint warnings in enterprise_project.py

--- src/main/python/uc3m_consulting/enterprise_project.py
+++ src/main/python/uc3m_consulting/enterprise_project.py
@@ -1,3 +1,4 @@
+"""Module containing the EnterpriseProject class"""
 import hashlib
 import json
 from datetime import datetime, timezone
@@ -8,7 +9,11 @@ from uc3m_consulting.attributes.attribute_budget import ProjectBudget
 from uc3m_consulting.attributes.attribute_cif import CompanyCif
 from uc3m_consulting.attributes.attribute_starting_date import StartingDate
 
+
 class EnterpriseProject:
+    """Class representing a project"""
+
+    #pylint: disable=too-many-arguments, too-many-positional-arguments
     def __init__(self,
                  company_cif: str,
                  project_acronym: str,
@@ -16,21 +21,22 @@ class EnterpriseProject:
                  department: str,
                  starting_date: str,
                  project_budget: float):
-        
+        """Initializes a new EnterpriseProject instance"""
         self.__company_cif = CompanyCif(company_cif).value
         self.__project_description = ProjectDescription(project_description).value
         self.__project_achronym = ProjectAcronym(project_acronym).value
         self.__department = ProjectDepartment(department).value
         self.__starting_date = StartingDate(starting_date).value
         self.__project_budget = ProjectBudget(project_budget).value
-        
         justnow = datetime.now(timezone.utc)
         self.__time_stamp = datetime.timestamp(justnow)
 
     def __str__(self):
+        """Returns string representation of the project"""
         return "Project:" + json.dumps(self.__dict__)
 
     def to_json(self):
+        """Returns the object information in json format"""
         return {
             "company_cif": self.__company_cif,
             "project_description": self.__project_description,
@@ -44,12 +50,15 @@ class EnterpriseProject:
 
     @property
     def project_id(self):
+        """Returns the md5 signature of the project"""
         return hashlib.md5(str(self).encode()).hexdigest()
 
     @property
     def company_cif(self):
+        """Property representing the company CIF"""
         return self.__company_cif
 
     @property
     def starting_date(self):
+        """Property representing the project starting date"""
         return self.__starting_date
